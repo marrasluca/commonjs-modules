d041cfb435bbd75523953c5b3a09882d1b248652
marrasluca <lmarras10@gmail.com>
2025-02-09 10:48:58 +0100

    end

--- people.js
+++ people.js
@@ -2,3 +2,14 @@
 const creaNome = require('./names')
 const creaHobbies = require('./hobbies')
 
+function creaPersona() {
+    const fullName = creaNome("Luca", "Marras")
+    const hobbies = creaHobbies("Viaggiare", "Leggere", "Basket") 
+
+    return {
+        fullName: fullName,
+        hobbies: hobbies
+    };
+}
+
+console.log(creaPersona());

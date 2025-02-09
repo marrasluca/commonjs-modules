b6f1ba2bff23574e78c56172fb916cd9f82dce67
marrasluca <lmarras10@gmail.com>
2025-02-09 10:40:20 +0100

    add: hobbies

---
+++ hobbies.js
@@ -0,0 +1,6 @@
+// Funzione che accetta tre parametri e restituisce un array di hobby
+function creaHobbies(hobbyOne, hobbyTwo, hobbyThree) {
+    return [hobbyOne, hobbyTwo, hobbyThree];
+}
+
+module.exports = creaHobbies;
